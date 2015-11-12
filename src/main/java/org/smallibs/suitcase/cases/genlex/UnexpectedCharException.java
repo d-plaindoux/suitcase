c65b153ec1b16cc306a02c8ca21d66870b877ba1
Didier Plaindoux <d.plaindoux@free.fr>
2015-11-12 09:29:55 +0100

    Fix error / keyboard side effect because of problem between the chair and the keyboard

--- src/main/java/org/smallibs/suitcase/cases/genlex/UnexpectedCharException.java
+++ src/main/java/org/smallibs/suitcase/cases/genlex/UnexpectedCharException.java
@@ -20,6 +20,6 @@ package org.smallibs.suitcase.cases.genlex;
 
 public class UnexpectedCharException extends Exception {
     public UnexpectedCharException(int index, char c) {
-        sper("char <" + c + "> at position <" + index + ">");
+        super("char <" + c + "> at position <" + index + ">");
     }
 }
